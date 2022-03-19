02ec46a9e5ae5f51441da894c1e9f2afaec54641
Sinchana2002 <83580216+Sinchana2002@users.noreply.github.com>
2022-03-19 18:50:43 +0530

    Add files via upload

---
+++ 05_file4.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+    FILE *fp;
+    char ch;
+    fp=fopen("sample.txt","w");
+    if(fp==NULL)
+    {
+        printf("error in opening a file");
+        exit(0);
+    }
+    printf("input some text");
+    while((ch=getchar()) !=EOF)
+        fputc(ch,fp);
+    fclose(fp);
+    printf("the file contains");
+    fp=fopen("sample.txt","r");
+    while((ch=fgetc(fp)) !=EOF)
+        putchar(ch);
+    fclose(fp);
+}
